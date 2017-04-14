9e6aef5c1df1026c598a436060a03c41b81bce42
Juaquin Sanchez <jnikksanchez@gmail.com>
2017-04-14 01:00:52 -0400

    persistent bartender drinks

--- bartender/bartender.js
+++ bartender/bartender.js
@@ -1,180 +1,209 @@
-var data = {reviews:[{}]};
+var data = { reviews: [{}], onShift: [], specialtyDrinks: [{}], };
 
 //a data structure for storing information about the team
 var tutorial_data = {
-	// developer: "Juaquin Sanchez",
-	// objects representing sticky notes
-	reviews: [
-		{
-			item: "Hurricane",
-			type: "beverage",
-			text: "Great drink and fast service, will come again!",
-			stars: 5,
-			response: '',
-		},
-		{
-			item: "Negroni",
-			type: "beverage",
-			text: "This sucks and you sucks",
-			stars: 2,
-			response: '',
-		},
-	],
+    reviews: [{
+        item: "Hurricane",
+        type: "beverage",
+        text: "Great drink and fast service, will come again!",
+        stars: 5,
+        response: '',
+    }, {
+        item: "Negroni",
+        type: "beverage",
+        text: "This sucks and you sucks",
+        stars: 2,
+        response: '',
+    },],
 
-	onShift: ['Juaquin', 'Connor', 'Ayo'],
+    onShift: ['Juaquin', 'Connor', 'Ayo'],
 
-	specialtyDrinks: [
-		{
-			name: "Hurricane",
-			desc: "The hurricane is a sweet alcoholic drink made with rum, fruit juice, and syrup or grenadine. It is one of many popular drinks served in New Orleans.",
-			null: NaN,
-		}
-	],
+    specialtyDrinks: [{
+        name: "Hurricane",
+        desc: "sweet alcoholic drink made with rum, fruit juice, and syrup or grenadine",
+        null: NaN,
+    }, {
+        name: "Tequila Sunrise",
+        desc: "cocktail made with tequila, orange juice, and grenadine syrup",
+        null: NaN,
+    }],
+};
+
+// Usage: 
+// "{0} is dead, but {1} is alive! {0} {2}".format("ASP", "ASP.NET")
+String.prototype.format = function() {
+    var str = this;
+    for (var i = 0; i < arguments.length; i++) {
+        var reg = new RegExp("\\{" + i + "\\}", "gm");
+        str = str.replace(reg, arguments[i]);
+    }
+    return str;
+};
+// alert("{0} is dead, but {1} is alive! {0} {2}".format("ASP", "ASP.NET"));
+
+function displayBarReviews() {
+    //get data object from localStorage, else loads default
+    data = JSON.parse(localStorage.getItem('data_spirit_webapp'));
+    if (!data) { data = tutorial_data; }
+
+    // add reviews in data object to HTML, 
+    // eg generating HTML to inject in page
+    var reviewsHTML = "";
+    for (var i = 0; i < data.reviews.length; i++) {
+        reviewsHTML += '<div class="list-group-item list-group-item-action flex-column align-items-start"><div class="d-flex w-100 justify-content-between"><h5 class="mb-1">';
+        reviewsHTML += data.reviews[i].item;
+        reviewsHTML += '</h5><h2>' + data.reviews[i].stars + '/5</h2></div>';
+        reviewsHTML += '<p class="mb-1">' + data.reviews[i].text + '</p>';
+        // reviewsHTML += '<div class="d-flex w-100 justify-content-between"></div>';
+        reviewsHTML += '<button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModal" data-index=' + i + ' id="exampleModal">Respond</button></div>';
+
+        // if there is a response, want to display it
+        var r = data.reviews[i].response;
+        if (r) {
+            reviewsHTML += '<div class="d-flex w-100 justify-content-between" id="review_response"><small>' + r + '</small></div>';
+        }
+    }
+    // puts generated html in wrapper
+    $("#insert_reviews").html(reviewsHTML);
 }
 
+function displayBartenderDrinks() {
+    data = JSON.parse(localStorage.getItem('data_spirit_webapp'));
+    if (!data) { data = tutorial_data; }
+    var barFavHTML = "<li>";
 
-function displayBarReviews () {
-	//get data object from localStorage, else loads default
-	data = JSON.parse(localStorage.getItem('data_spirit_webapp'));
-	if(!data) {data = tutorial_data;}
+    var drink = "<div class='list-group'> <div class='list-group-item list-group-item-action flex-column align-items-start'>" +
+        "<div class='d-flex w-100 justify-content-between'> <h5 class='mb-1'>" +
+        "<textarea id='drinkName' rows='1' cols='50' placeholder='Insert Drink Name' style = 'border: none; resize: none;' maxlength='35'>{0}</textarea>" +
+        "</h5></div><p class='mb-1'><textarea id='description' rows='2' cols='100' placeholder='Insert Drink Description (max 100 characters)' style = 'border: none; resize: none;' maxlength='100'>{1}</textarea></p>" +
+        "<div><button class='btn btn-secondary my-1' onclick='saveDrink()'>Save</button>" +
+        "<button class='btn btn-secondary my-1' onclick='removeDrink(this)'>Delete</button></div></div></div>";
 
-	// add reviews in data object to HTML, 
-	// eg generating HTML to inject in page
-	var reviewsHTML = "";
-	for (var i = 0; i < data.reviews.length; i++) {
-		reviewsHTML += '<div class="list-group-item list-group-item-action flex-column align-items-start"><div class="d-flex w-100 justify-content-between"><h5 class="mb-1">';
-		reviewsHTML += data.reviews[i].item;
-		reviewsHTML += '</h5><h2>' + data.reviews[i].stars + '/5</h2></div>';
-		reviewsHTML += '<p class="mb-1">' + data.reviews[i].text + '</p>'
-		// reviewsHTML += '<div class="d-flex w-100 justify-content-between"></div>';
-		
-		reviewsHTML += '<button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModal" data-index=' + i + ' id="exampleModal">Respond</button></div>';
-
-		var r = data.reviews[i].response;
-		if(r) {
-			reviewsHTML += '<div class="d-flex w-100 justify-content-between" id="review_response"><small>' + r + '</small></div>';
-		};
-
-	}
-	// puts generated html in wrapper
-	$("#insert_reviews").html(reviewsHTML);
-
-	// $('string/element/array/function/jQuery object/string, context')("#").click(function() { 
- //        incrementPopularity($(this).attr("data-index"))
- //    });
+    for (var i = 0; i < data.specialtyDrinks.length; i++) {
+        var drinkObj = data.specialtyDrinks[i];
+        barFavHTML += drink.format(drinkObj.name, drinkObj.desc);
+    }
+    barFavHTML += '</li>';
+    $('#specialty_drink').html(barFavHTML);
 
 }
 
-function displayBartenderDrinks () {
-	data = JSON.parse(localStorage.getItem('data_spirit_webapp'));
-	if(!data) {data = tutorial_data;}
-	var barFavHTML = "";
+function saveDrink() {
+    // update data.specialtyDrinks
+    data.specialtyDrinks = [{}];
+    var names = document.querySelectorAll('#drinkName');
+    var descs = document.querySelectorAll('#description');
+    for (var i = 0; i < names.length; i++) {
+    	n = names[i].value;
+    	d = descs[i].value;
+    	data.specialtyDrinks[i] = {name: n, desc: d};
+    }
+    console.log(data.specialtyDrinks);
+    // save to localStorage
+    localStorage.setItem('data_spirit_webapp', JSON.stringify(data));
+    displayBartenderDrinks();
+}
 
-	var drink = "<div class='list-group'> <div class='list-group-item list-group-item-action flex-column align-items-start'>" +
-                    "<div class='d-flex w-100 justify-content-between'> <h5 class='mb-1'>"+
-                    "<textarea rows='1' cols='50' placeholder='Insert Drink Name' style = 'border: none; resize: none;' maxlength='35'></textarea>" +
-                    "</h5></div><p class='mb-1'><textarea rows='2' cols='100' placeholder='Insert Drink Description (max 100 characters)' style = 'border: none; resize: none;' maxlength='100'></textarea></p>" +
-                    "<div><button class='btn btn-secondary my-1' onclick=''>Save</button>"+
-                    "<button class='btn btn-secondary my-1' onclick='removeDrink(this)'>Delete</button></div></div></div>";
+function addDrink() {
+    var drink = "<div class='list-group'> <div class='list-group-item list-group-item-action flex-column align-items-start'>" +
+        "<div class='d-flex w-100 justify-content-between'> <h5 class='mb-1'>" +
+        "<textarea id='drinkName' rows='1' cols='50' placeholder='Insert Drink Name' style = 'border: none; resize: none;' maxlength='35'></textarea>" +
+        "</h5></div><p class='mb-1'><textarea id='description' rows='2' cols='100' placeholder='Insert Drink Description (max 100 characters)' style = 'border: none; resize: none;' maxlength='100'></textarea></p>" +
+        "<div><button class='btn btn-secondary my-1' onclick='saveDrink()'>Save</button>" +
+        "<button class='btn btn-secondary my-1' onclick='removeDrink(this)'>Delete</button></div></div></div>";
 
-	for (var i = 0; i < data.specialtyDrinks.length; i++) {
-		data.specialtyDrinks[i]
-	}
+    document.getElementById("specialty_drink").insertAdjacentHTML('beforeend', drink);
+}
 
+function removeDrink(e) {
+    e.parentNode.parentNode.remove();
 }
 
 function displayOnShiftHome() {
-	data = JSON.parse(localStorage.getItem('data_spirit_webapp'));
-	if(!data) {data = tutorial_data;}
-	var homeShiftHTML = "";
-	homeShiftHTML += '<h1>On Shift: '
-	for (var i = 0; i < data.onShift.length; i++) {
-		homeShiftHTML += data.onShift[i] + ', '
-	}
-	end = homeShiftHTML.length;
-	homeShiftHTML = homeShiftHTML.slice(0,end-2);
-	homeShiftHTML += '</h1>';
-	$("#onShiftHome").html(homeShiftHTML);
+    data = JSON.parse(localStorage.getItem('data_spirit_webapp'));
+    if (!data) { data = tutorial_data; }
+    var homeShiftHTML = "";
+    homeShiftHTML += '<h1>On Shift: ';
+    for (var i = 0; i < data.onShift.length; i++) {
+        homeShiftHTML += data.onShift[i] + ', ';
+    }
+    end = homeShiftHTML.length;
+    homeShiftHTML = homeShiftHTML.slice(0, end - 2);
+    homeShiftHTML += '</h1>';
+    $("#onShiftHome").html(homeShiftHTML);
 }
 
-function displayCheckedShifts () {
-	data = JSON.parse(localStorage.getItem('data_spirit_webapp'));
-	if(!data) {data = tutorial_data;}
-	for (var i = 0; i < data.onShift.length; i++) {
-		$('#'+data.onShift[i])[0].checked = true;
-	}
-	console.log(data.onShift);
+function displayCheckedShifts() {
+    data = JSON.parse(localStorage.getItem('data_spirit_webapp'));
+    if (!data) { data = tutorial_data; }
+    for (var i = 0; i < data.onShift.length; i++) {
+        $('#' + data.onShift[i])[0].checked = true;
+    }
+    console.log(data.onShift);
 }
-
-// barProfileSubmit
-$("body").on('click', '#saveBarProfile', function () {
-	// get checked items
-	var all = $(":checkbox")
-	data.onShift = [];
-	for (var i = 0; i < all.length; i++) {
-		
-		if (all[i].checked) {
-			data.onShift.push(all[i].id)
-		}
-	}
-	localStorage.setItem('data_spirit_webapp', JSON.stringify(data));
-	displayCheckedShifts();
-})
 
 function addReview(text, index) {
-	data.reviews[index].response = text; 
-	localStorage.setItem('data_spirit_webapp', JSON.stringify(data));
-	displayBarReviews();
+    data.reviews[index].response = text;
+    localStorage.setItem('data_spirit_webapp', JSON.stringify(data));
+    displayBarReviews();
 }
 
-$("body").on('click','#exampleModal',function(){
-	$("#Modal").modal("show");
-	global_index = $(this).attr("data-index");
+function barProfileSubmit() {
+    // get checked items
+    var all = $(":checkbox");
+    data.onShift = [];
+    for (var i = 0; i < all.length; i++) {
+
+        if (all[i].checked) {
+            data.onShift.push(all[i].id);
+        }
+    }
+    localStorage.setItem('data_spirit_webapp', JSON.stringify(data));
+    displayCheckedShifts();
+}
+
+
+$("body").on('click', '#saveBarProfile', function() {
+    barProfileSubmit();
 });
 
-$("body").on('click','#close_modal #close',function(){
-	$("#Modal").hide();//modal("hide");
-	$('body').removeClass('modal-open');
-	$('.modal-backdrop').remove();
-	displayBarReviews();
-	location.reload();
-
+$("body").on('click', '#exampleModal', function() {
+    $("#Modal").modal("show");
+    global_index = $(this).attr("data-index");
 });
 
-$("body").on('click','#save_changes',function(){
-	text = $("#Modal #message-text")["0"].value
-	$("#Modal").hide();
-	$('body').removeClass('modal-open');
-	$('.modal-backdrop').remove();
-	addReview(text, global_index);
-	location.reload();
+$("body").on('click', '#close_modal #close', function() {
+    $("#Modal").hide(); //modal("hide");
+    $('body').removeClass('modal-open');
+    $('.modal-backdrop').remove();
+    displayBarReviews();
+    location.reload();
+});
+
+$("body").on('click', '#save_changes', function() {
+    text = $("#Modal #message-text")["0"].value;
+    $("#Modal").hide();
+    $('body').removeClass('modal-open');
+    $('.modal-backdrop').remove();
+    addReview(text, global_index);
+    location.reload();
 });
 
 
 // and awaaaaay we go
 $(function() {
 
-	String.prototype.format = function() {
-	var args = arguments;
-	return this.replace(/{(\d+)}/g, function(match, number) { 
-	  return typeof args[number] != 'undefined'
-	    ? args[number]
-	    : match
-	  ;
-	});
-	}
-
-	page = window.location.pathname.split("/").pop();
-	if (page == "review.html") {
-		displayBarReviews();
-	}
-	if (page == "bar_profile.html") {
-		displayCheckedShifts();
-	}
-	if (page == "home.html") {
-		displayOnShiftHome();
-	}
-	if (page == "favorites.html") {
-		displayBartenderDrinks();
-	}
+    page = window.location.pathname.split("/").pop();
+    if (page == "review.html") {
+        displayBarReviews();
+    }
+    if (page == "bar_profile.html") {
+        displayCheckedShifts();
+    }
+    if (page == "home.html") {
+        displayOnShiftHome();
+    }
+    if (page == "favorites.html") {
+        displayBartenderDrinks();
+    }
 });
